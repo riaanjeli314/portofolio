57b33468bf9a7afaa1bba20d8827091a2626ebdd
riaanjeli314 <12150120314@students.uin-suska.ac.id>
2023-03-28 15:12:42 +0700

    latihan vue

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <hi>WELCOME TO KUDA GILA WORLD </hi>
+  <img src="https://st.depositphotos.com/1034986/4142/i/600/depositphotos_41420759-stock-photo-man-in-horse-mask.jpg">
+</template>
+
